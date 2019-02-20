7d6ed2093e7759713360f29180846cfa27e637e7
Joel Hernandez <jherna72@ucsc.edu>
2019-02-20 00:26:53 -0800

    UnityProject

---
+++ Animation/Assets/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float Speed = 5f;
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "Capsule")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Speed, 0, 0);
+    }
+
+}
